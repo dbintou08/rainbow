ff523916db65c3bbc3c8685e5bcd748662173f21
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2025-03-07 07:47:42 +0000

    Implement JWT authentication in User model and update AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,6 +15,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
+            // 'date_of_birth' => 'required|string',
             'password' => 'required|string|min:6',
         ]);
 
